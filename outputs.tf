eef4dbc08f3a691a78e555561785b97af3356c47
Rishiryadav <93231025+Rishiryadav@users.noreply.github.com>
2024-04-12 15:55:48 +0800

    Add files via upload
    Update the Stack files

---
+++ outputs.tf
@@ -0,0 +1,23 @@
+output prod-ml-model-predict-endpoint {
+  value = "${oci_apigateway_deployment.prod-ml-model.endpoint}/predict"
+}
+
+output test-ml-model-predict-endpoint {
+  value = "${oci_apigateway_deployment.test-ml-model.endpoint}/predict"
+}
+
+output devops-code-repo-url {
+    value = oci_devops_repository.mlops-code-repo.http_url
+}
+
+output ml-model-container-repo-path {
+    value = "${var.ocir_url}/${data.oci_objectstorage_namespace.os_namespace.namespace}/${oci_artifacts_container_repository.mlops_model.display_name}"
+}
+
+output container-reg-auth-token-id {
+    value = oci_vault_secret.container-registry-auth-token.id
+}
+
+output apex-url {
+  value = oci_database_autonomous_database.mlops-adb-apex.connection_urls[*].apex_url
+}
